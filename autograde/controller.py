3057e206315ba68406dfc8bd7e962c92985eb453
kaushiksrini <kaushik.srinii@gmail.com>
2020-06-07 16:47:37 -0400

    modified test file

--- autograde/controller.py
+++ autograde/controller.py
@@ -1,10 +1,31 @@
 from .utils import wrap_message, get_file_path
 import difflib
+from .arguments import FileArgument
 
 
 class TestController(object):
     def __init__(self):
         pass
+
+    def get_file_diff(self, test_name, file1, file2):
+        expectedLines = open(file1).readlines()
+        outputLines = open(file2).readlines()
+        msg = ''
+        diffOut = ''
+
+        for line in difflib.unified_diff(expectedLines, outputLines):
+            diffOut += line
+
+        if len(diffOut) > 0:
+            msg += ('FAIL -- Test %s has different ouput than expected' %
+                    (test_name))
+
+            msg += (wrap_message(''.join(expectedLines),
+                                 test_name + ' Expected Lines'))
+            msg += (wrap_message(''.join(outputLines),
+                                 test_name + ' Output Lines'))
+
+        return msg
 
 
 class DiffTester(object):
@@ -27,7 +48,7 @@ class DiffTester(object):
         return msg
 
 
-class DiffExpectedFileTester(object):
+class DiffExpectedFileTester(TestController):
     def __init__(self, file_name='', return_value=0):
         self.file_name = file_name
         self.return_value = return_value
@@ -38,23 +59,8 @@ class DiffExpectedFileTester(object):
             msg += 'FAIL (RETURN CODE) -- "%s" compiled successfully but returned [%d] when run instead of [%d].\n' % (
                 target, return_code, self.return_value)
 
-        expectedLines = open(get_file_path(
-            "expected", self.file_name)).readlines()
-        outputLines = open(get_file_path("output", self.file_name)).readlines()
-
-        diffOut = ''
-
-        for line in difflib.unified_diff(expectedLines, outputLines):
-            diffOut += line
-
-        if len(diffOut) > 0:
-            msg += ('FAIL -- Test %s has different ouput than expected' %
-                    (test_name))
-
-            msg += (wrap_message(''.join(expectedLines),
-                                 test_name + ' Expected Lines'))
-            msg += (wrap_message(''.join(outputLines),
-                                 test_name + ' Output Lines'))
+        msg += self.get_file_diff(test_name, get_file_path(
+            "expected", self.file_name), get_file_path("output", self.file_name))
 
         if len(msg) > 0:
             return msg
@@ -64,19 +70,29 @@ class DiffExpectedFileTester(object):
     # now check whether the files are the same
 
 
-class ReturnValueController(object):
-    def __init__(self, return_value=0, non_zero=False):
+class ReturnValueController(TestController):
+    def __init__(self, return_value=0, non_zero=False, output_file=''):
         self.return_value = return_value
         self.non_zero = non_zero
+        self.output_file = output_file
 
     def generate_msg(self, test_name, target, out, err, return_code):
         msg = ''
         if self.non_zero and return_code == 0:
             msg += 'FAIL (RETURN CODE) -- "%s" compiled successfully but returned [%d] when run instead of a non-zero value.\n' % (
                 target, return_code)
-        elif self.return_value != return_code:
+        elif not self.non_zero and self.return_value != return_code:
             msg += 'FAIL (RETURN CODE) -- "%s" compiled successfully but returned [%d] when run instead of [%d].\n' % (
                 target, return_code, self.return_value)
+
+        if self.output_file:
+            if isinstance(self.output_file, str):
+                msg += self.get_file_diff(test_name, get_file_path(
+                    "expected", self.output_file), get_file_path("output", self.output_file))
+
+            if isinstance(self.output_file, FileArgument):
+                msg += self.get_file_diff(test_name, self.output_file.get_mount(
+                    folder='expected'), self.output_file.get_mount(folder='output'))
 
         if len(msg) > 0:
             return msg
